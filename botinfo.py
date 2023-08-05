2229a3e5f1d88f2ae716b683eedda76502ad4d77
lilbud <bvw20002010@gmail.com>
2023-08-05 17:00:13 -0400

    update

--- botinfo.py
+++ botinfo.py
@@ -8,7 +8,7 @@ async def botHelp(ctx):
   embed.add_field(name="Cover:\t`!cover YYYY-MM-DD`", value="Will get artwork from my website for that show", inline=False)
   embed.add_field(name="Jungleland:\t`!jl YYYY-MM-DD`", value="Returns link to Jungleland torrents with specified date", inline=False)
   embed.add_field(name="On This Day:\t`!otd`  OR  `!otd MM-DD`", value="Returns a list of shows that happened on this day. Can leave blank for the current day, or enter a specific Month and Day", inline=False)
-  embed.add_field(name="Artwork:\t`!artwork YYYY-MM-DD`", value="Returns list of artwork from Jungleland.it", inline=False)
+  #embed.add_field(name="Artwork:\t`!artwork YYYY-MM-DD`", value="Returns list of artwork from Jungleland.it", inline=False)
   embed.add_field(name="Bootleg:\t`!bootleg YYYY-MM-DD`", value="Returns link to SpringsteenLyrics with list of Bootlegs for that date", inline=False)
   embed.add_field(name="Info:\t`!info`", value="Returns Info on this Bot", inline=False)
   embed.add_field(name="Song Finder:\t`!song [SONG NAME]`", value="searches Brucebase for the requested song, returns a link as well as number of times it has been played.", inline=False)
