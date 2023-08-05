5bca1f58f2f68b2058a94983f424cefb96eac187
lilbud <bvw20002010@gmail.com>
2023-08-05 18:29:28 -0400

    Update setlistFinder.py

--- setlistFinder.py
+++ setlistFinder.py
@@ -23,15 +23,16 @@ async def setlistFinder(ctx, date):
             #premiere = cur.execute("""SELECT * FROM EVENTS WHERE (setlist LIKE '%' || %s || '%') ORDER BY event_id ASC""", (song,)).fetchone()
             #bustout = cur.execute("""SELECT * FROM EVENTS WHERE tour = %s AND tour != '' AND setlist LIKE '%""" + song + "%' ORDER BY event_id ASC", (r[5],)).fetchone()
   
-            if premiere[2] == r[2]:
-              setL.append(t[0] + " **[" + str(2) + "]**")
-            elif bustout:
-              if bustout[2] == r[2]:
-                setL.append(t[0] + " **[" + str(1) + "]**") 
-              else:
-                setL.append(t[0])
-            else:
-              setL.append(t[0])
+            # if premiere[2] == r[2]:
+            #   setL.append(t[0] + " **[" + str(2) + "]**")
+            # elif bustout:
+            #   if bustout[2] == r[2]:
+            #     setL.append(t[0] + " **[" + str(1) + "]**") 
+            #   else:
+            #     setL.append(t[0])
+            # else:
+            #   setL.append(t[0])
+            setL.append(t[0])
   
           setlist = ", ".join(setL)
     
