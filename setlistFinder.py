696f4c83ff1912b2baba2947b80c487c30ea46a2
lilbud <bvw20002010@gmail.com>
2023-08-05 23:10:37 -0400

    Deployment commit

--- setlistFinder.py
+++ setlistFinder.py
@@ -32,12 +32,12 @@ async def setlistFinder(ctx, date):
               if premiere[0] == r[2]:
                 setL.append(t[0] + " **[2]**")
               else:
-                setL.append(len(premiere[0]))
+                setL.append(str(len(premiere[0])))
             elif bustout:
               if bustout[0] == r[2]:
                 setL.append(t[0] + " **[1]**")
               else:
-                setL.append(len(bustout[0]))
+                setL.append(str(len(bustout[0])))
             else:
               setL.append(t[0])
             #setL.append(premiere[0], bustout[0])
