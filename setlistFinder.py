37498a4b25120597362b6c75e68163dfd6ed75f1
lilbud <bvw20002010@gmail.com>
2023-08-05 22:43:54 -0400

    Deployment commit

--- setlistFinder.py
+++ setlistFinder.py
@@ -13,7 +13,9 @@ async def setlistFinder(ctx, date):
         embed.add_field(name="", value="[" + r[1] + "](" + mainURL + r[2] + ")\n*" + r[3] + "*", inline=False)
         embed.set_footer(text=r[5])
 
-        for s in cur.execute("""SELECT DISTINCT ON (set_type) * FROM SETLISTS WHERE event_url = %s ORDER BY setlist_song_id ASC""", (r[2],)).fetchall():
+        #SELECT DISTINCT ON (set_type) * FROM SETLISTS WHERE event_url = %s ORDER BY setlist_song_id ASC
+
+        for s in cur.execute("""SELECT * FROM (SELECT DISTINCT ON (set_type) * FROM SETLISTS WHERE event_url=%s ORDER BY set_type, setlist_song_id ASC) ORDER BY setlist_song_id ASC""", (r[2],)).fetchall():
           setL = []
           key = ""
           temp = cur.execute("""SELECT song_name, song_url FROM SETLISTS WHERE event_url = %s AND set_type = %s ORDER BY song_num ASC""", (r[2], s[0],)).fetchall()
