9f54057bd69b90cf886b97c0b2591f0f24926fcf
lilbud <bvw20002010@gmail.com>
2023-08-05 22:08:34 -0400

    Deployment commit

--- setlistFinder.py
+++ setlistFinder.py
@@ -20,9 +20,9 @@ async def setlistFinder(ctx, date):
       
           for t in temp:
             #song = t[0].replace("'", "''")
-            song = t[0]
+            song = "'%" + t[0] + "%'"
             premiere = cur.execute("""SELECT * FROM EVENTS WHERE setlist LIKE '%""" + date + "%' ORDER BY event_id ASC").fetchone() #ORDER BY event_id ASC
-            bustout = cur.execute("""SELECT * FROM EVENTS WHERE tour = %s AND tour != '' AND setlist LIKE '%""" + song + "%' ORDER BY event_id ASC", (r[5],)).fetchone()
+            bustout = cur.execute("""SELECT * FROM EVENTS WHERE tour = %s AND tour != '' AND setlist LIKE %s ORDER BY event_id ASC""", (r[5],song,)).fetchone()
   
             if premiere[2] == r[2]:
               setL.append(t[0] + " **[" + str(2) + "]**")
