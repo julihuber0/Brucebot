cb46de340008d808938ec0ffbd8f4fb4984b83e5
lilbud <bvw20002010@gmail.com>
2023-08-05 23:43:16 -0400

    Deployment commit

--- setlistFinder.py
+++ setlistFinder.py
@@ -24,11 +24,11 @@ async def setlistFinder(ctx, date):
             #setL = []
             premiere = bustout = ""
             # song = t[0].replace("'", "''")
-            song = "%" + t[0].replace("'", "''") + "%"
+            song = "'%" + t[0].replace("'", "''") + "%'"
             date = "'%" + date + "%'"
-            
-            premiere = cur.execute("""SELECT event_url FROM EVENTS WHERE setlist LIKE '%s' ORDER BY event_id ASC""", (song,)).fetchone() #ORDER BY event_id ASC
-            bustout = cur.execute("""SELECT event_url FROM EVENTS WHERE tour = %s AND tour != '' AND setlist LIKE '%s' ORDER BY event_id ASC""", (r[5],song,)).fetchone()
+
+            premiere = cur.execute("""SELECT event_url FROM EVENTS WHERE setlist LIKE %s ORDER BY event_id ASC""", (song,)).fetchone() #ORDER BY event_id ASC
+            bustout = cur.execute("""SELECT event_url FROM EVENTS WHERE tour = %s AND tour != '' AND setlist LIKE %s ORDER BY event_id ASC""", (r[5],song,)).fetchone()
 
             if premiere and s[5] not in ["Soundcheck", "Rehearsal"]:
               if premiere[0] == r[2]:
