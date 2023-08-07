10c79f7ce63a2f5f3498e2fee6feff39c5f6cd67
lilbud <bvw20002010@gmail.com>
2023-08-07 18:05:53 -0400

    Deployment commit

--- setlistFinder.py
+++ setlistFinder.py
@@ -32,7 +32,7 @@ async def setlistFinder(ctx, date=None):
               bustout: 
               
               """
-              premiere = cur.execute("""SELECT event_url FROM SETLISTS WHERE song_name IS NOT DISTINCT FROM %s AND set_type != ANY(%s) ORDER BY setlist_song_id ASC""", (t[0],['Soundcheck', 'Rehearsal'],)).fetchone()
+              premiere = cur.execute("""SELECT event_url FROM SETLISTS WHERE song_name IS NOT DISTINCT FROM %s AND set_type NOT IN %s ORDER BY setlist_song_id ASC""", (t[0],('Soundcheck', 'Rehearsal'),)).fetchone()
               #premiere = cur.execute("""SELECT event_url FROM SETLISTS WHERE song_name IS NOT DISTINCT FROM %s ORDER BY setlist_song_id ASC""", (t[0],)).fetchone()
               bustout = None
 
