34e432d991f3b6300c4cdb46071514bf222de2b2
lilbud <bvw20002010@gmail.com>
2023-08-09 21:35:13 -0400

    Deployment commit

--- songFinder.py
+++ songFinder.py
@@ -8,7 +8,7 @@ async def sFind(ctx, *song):
   if len(" ".join(song)) > 1:
     song_name = "'%" + " ".join(song).replace("'", "''") + "%'"
 
-    s = cur.execute("""SELECT * FROM SONGS WHERE song_name ILIKE """ + song_name).fetchone()
+    s = cur.execute("""SELECT * FROM SONGS WHERE song_name ILIKE %s""", (song_name,)).fetchone()
 
     if s:
       f = cur.execute("""SELECT event_url FROM EVENTS WHERE event_date = %s""", (s[3],)).fetchone()
