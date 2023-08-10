ec928d8169a8248c44024d0e6a5ebb671e4f4383
lilbud <bvw20002010@gmail.com>
2023-08-10 01:40:35 -0400

    Update bootleg.py

--- bootleg.py
+++ bootleg.py
@@ -4,10 +4,11 @@ from importStuff import *
 async def bootlegFind(ctx, date=None):
 
   if dateChecker(date) and date is not None:
-    event_name = cur.execute("SELECT event_name FROM EVENTS WHERE event_date=%s", (date,)).fetchone()
+    event_name = cur.execute("SELECT event_date, event_venue, event_city, event_state, event_country, show FROM EVENTS WHERE event_date=%s", (date,)).fetchone()
 
     if event_name:
-      embed = createEmbed("Bootlegs For: " + date, event_name[0])
+      location = ", ".join(list(filter(None, event_name[1:])))
+      embed = createEmbed("Bootlegs For: " + date, location)
       URL = "https://www.springsteenlyrics.com/bootlegs.php?filter_date=%s&cmd=list&category=filter_date" % date
       embed.add_field(name="", value = "[SpringsteenLyrics](" + URL + ")")
       await ctx.send(embed=embed)
