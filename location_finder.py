b0d6b9d9904c74a5080efd62cc35c87cbba7857e
lilbud <bvw20002010@gmail.com>
2023-08-10 23:09:49 -0400

    Deployment commit

--- location_finder.py
+++ location_finder.py
@@ -22,18 +22,21 @@ async def city_finder(ctx, *city):
 
 @bot.command(aliases=['state'])
 async def state_finder(ctx, state):
-    if len(state) == 2:
-        state_name = state
-        events = cur.execute(f"""SELECT event_date, event_url, event_state FROM EVENTS WHERE LOWER(event_state) LIKE '%{state}%' AND setlist != '' ORDER BY event_id ASC""").fetchall()
+    if state:
+        if len(state) == 2:
+            events = cur.execute(f"""SELECT event_date, event_url, event_state FROM EVENTS WHERE LOWER(event_state) LIKE '%{state}%' ORDER BY event_id ASC""").fetchall()
+            last = cur.execute(f"""SELECT event_date, event_url, event_state FROM EVENTS WHERE LOWER(event_state) LIKE '%{state}%' AND setlist != '' ORDER BY event_id DESC""").fetchall()
 
-        if events:
-            embed = create_embed(f"Database Results for: {events[0][2]}", "")
-            embed.add_field(name="Number of Shows:", value=str(len(events)), inline=True)
-            embed.add_field(name="First Show:", value=f"[{events[0][0]}]({main_url}{events[0][1]})", inline=True)
-            embed.add_field(name="Last Show:", value=f"[{events[-1][0]}]({main_url}{events[-1][1]})", inline=True)
-            await ctx.send(embed=embed)
+            if events:
+                embed = create_embed(f"Database Results for: {events[0][2]}", "")
+                embed.add_field(name="Number of Shows:", value=str(len(events)), inline=True)
+                embed.add_field(name="First Show:", value=f"[{events[0][0]}]({main_url}{events[0][1]})", inline=True)
+                embed.add_field(name="Last Show:", value=f"[{last[0][0]}]({main_url}{last[0][1]})", inline=True)
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send(f"No Results for {state}")
         else:
-            await ctx.send(f"No Results for {state_name}")
+            await ctx.send(error_message('input'))
     else:
         await ctx.send(error_message('input'))
 
