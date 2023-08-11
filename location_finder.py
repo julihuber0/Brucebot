4fb28f8f4b26c99bb512e5a1b10c2bda43526785
lilbud <bvw20002010@gmail.com>
2023-08-10 22:59:19 -0400

    Deployment commit

--- location_finder.py
+++ location_finder.py
@@ -4,9 +4,7 @@ from error_message import error_message
 
 @bot.command(aliases=['city'])
 async def city_finder(ctx, *city):
-    print()
-
-    if city is not None:
+    if len(" ".join(city)) > 1:
         city_name = " ".join(city).replace("'", "''").lower()
         events = cur.execute(f"""SELECT event_date, event_url, event_city FROM EVENTS WHERE LOWER(event_city) LIKE '%{city_name}%' AND setlist != '' ORDER BY event_id ASC""").fetchall()
 
@@ -23,9 +21,35 @@ async def city_finder(ctx, *city):
 
 
 @bot.command(aliases=['state'])
-async def state_finder(ctx, *city):
-    print()
+async def state_finder(ctx, state):
+    if len(state) == 2:
+        state_name = " ".join(state).replace("'", "''").lower()
+        events = cur.execute(f"""SELECT event_date, event_url, event_state FROM EVENTS WHERE LOWER(event_state) LIKE '%{state}%' AND setlist != '' ORDER BY event_id ASC""").fetchall()
+
+        if events:
+            embed = create_embed(f"Database Results for: {events[0][2]}", "")
+            embed.add_field(name="Number of Shows:", value=str(len(events)), inline=True)
+            embed.add_field(name="First Show:", value=f"[{events[0][0]}]({main_url}{events[0][1]})", inline=True)
+            embed.add_field(name="Last Show:", value=f"[{events[-1][0]}]({main_url}{events[-1][1]})", inline=True)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send(f"No Results for {state_name}")
+    else:
+        await ctx.send(error_message('input'))
 
 @bot.command(aliases=['country'])
 async def country_finder(ctx, *city):
-    print()
+    if len(" ".join(country)) > 1:
+        country_name = " ".join(country).replace("'", "''").lower()
+        events = cur.execute(f"""SELECT event_date, event_url, event_country FROM EVENTS WHERE LOWER(event_country) LIKE '%{country_name}%' AND setlist != '' ORDER BY event_id ASC""").fetchall()
+
+        if events:
+            embed = create_embed(f"Database Results for: {events[0][2]}", "")
+            embed.add_field(name="Number of Shows:", value=str(len(events)), inline=True)
+            embed.add_field(name="First Show:", value=f"[{events[0][0]}]({main_url}{events[0][1]})", inline=True)
+            embed.add_field(name="Last Show:", value=f"[{events[-1][0]}]({main_url}{events[-1][1]})", inline=True)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send(f"No Results for {country_name}")
+    else:
+        await ctx.send(error_message('input'))
