973923c2d58b35270e72790e2a366edb576a2644
lilbud <bvw20002010@gmail.com>
2023-08-11 12:09:34 -0400

    Deployment commit

--- create_embed.py
+++ create_embed.py
@@ -7,5 +7,7 @@ from import_stuff import discord
 
 def create_embed(title, description):
     """Returns a Discord Embed with the provided title and des."""
+    embed = discord.Embed(title=title, description=description, color=0x6d3da4)
+    embed.set_author(name=ctx.author.display_name, icon_url=str(ctx.author.avatar.url))
 
-    return discord.Embed(title=title, description=description, color=0x6d3da4)
+    return embed
