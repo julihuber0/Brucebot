b6df390a242d7b3de98c71b91572b4cb10fe4030
lilbud <bvw20002010@gmail.com>
2023-08-10 20:07:20 -0400

    Deployment commit

--- bootleg.py
+++ bootleg.py
@@ -10,11 +10,11 @@ async def bootleg_find(ctx, date=None):
     """Returns a link to SpringsteenLyrics Bootleg Page for the specified Date"""
 
     if dateChecker(date) and date is not None:
-        event_name = cur.execute(f"""SELECT event_venue, event_city, event_state, event_country, show FROM EVENTS WHERE event_date={date}""").fetchone()
+        event_name = cur.execute(f"""SELECT event_venue, event_city, event_state, event_country, show FROM EVENTS WHERE event_date={str(date)}""").fetchone()
 
         if event_name:
             location = ", ".join(list(filter(None, event_name[0:])))
-            embed = createEmbed(f"Bootlegs For: \"{date}\"", location)
+            embed = createEmbed(f"Bootlegs For: {date}", location)
             url = f"https://www.springsteenlyrics.com/bootlegs.php?filter_date={date}&cmd=list&category=filter_date"
             embed.add_field(name="", value=f"[SpringsteenLyrics]({url})")
             await ctx.send(embed=embed)
