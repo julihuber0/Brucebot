2f2006eb45b60fca67847e3cd6665f5fbaf371dd
lilbud <bvw20002010@gmail.com>
2023-08-27 21:07:15 -0400

    Deployment commit

--- setlist_finder.py
+++ setlist_finder.py
@@ -34,7 +34,7 @@ async def setlist_finder(ctx, date=None):
 
 					for t in cur.execute(f"""SELECT song_name, song_url FROM SETLISTS WHERE event_url LIKE '%{r[2]}%' AND set_type LIKE '%{s[5].replace("'", "''")}%' ORDER BY song_num ASC""").fetchall():
 						premiere = cur.execute(f"""SELECT first_played FROM SONGS WHERE song_url LIKE '%{t[1]}%'""").fetchone()
-						bustout = cur.execute(f"""SELECT event_date FROM EVENTS WHERE tour LIKE '{r[9].replace("'", "''")}' AND setlist LIKE '%{t[0].replace("'", "''")}%' AND event_date LIKE '{r[1]}' ORDER BY event_id LIMIT 1""").fetchone()
+						bustout = cur.execute(f"""SELECT event_date FROM EVENTS WHERE tour = '{r[9].replace("'", "''")}' AND setlist LIKE '%{t[0].replace("'", "''")}%' AND event_date = '{r[1]}' ORDER BY event_id LIMIT 1""").fetchone()
 
 						#check setlist table for song url and tour, order by id ascending, if date equals r[1] (date) and tour = r[9], then bustout
 
