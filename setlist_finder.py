2a82d2a719e6761c273c5f114dadf5ca21668b9f
lilbud <bvw20002010@gmail.com>
2023-08-27 21:12:24 -0400

    Deployment commit

--- setlist_finder.py
+++ setlist_finder.py
@@ -42,8 +42,9 @@ async def setlist_finder(ctx, date=None):
 							if premiere[0] == r[1]:
 								set_l.append(f"{t[0]} **[1]**")
 							else:
-								set_l.append(t[0])
-						elif bustout and s[5] not in ['Soundcheck', 'Rehearsal']:
+								set_l.append(f"{t[0]} [PREMIERE]")
+						
+						if bustout and s[5] not in ['Soundcheck', 'Rehearsal']:
 							if bustout[0] == r[1]:
 								set_l.append(f"{t[0]} **[2]**")
 							else:
