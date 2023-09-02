6c00fbf18264100e9bdc1072e4fa5c0aca4feb41
lilbud <bvw20002010@gmail.com>
2023-09-01 22:39:23 -0400

    Deployment commit

--- song_finder.py
+++ song_finder.py
@@ -10,15 +10,15 @@ from error_message import error_message
 @bot.command(aliases=['song'])
 async def song_finder(ctx, *song):
 	"""Gets info on inputted song"""
-	#id, url, name, first_played, last_played, num_plays
-
+	
 	if len(" ".join(song)) > 1:
 		song_name = song_name_fix(" ".join(song).replace("'", "''").replace("\"", "''"))
+		#id, url, name, first_played, last_played, num_plays
 
-		if cur.execute(f"""SELECT * FROM SONGS WHERE LOWER(song_name) ILIKE '{song_name.lower()}'""").fetchone():
-			s = cur.execute(f"""SELECT * FROM SONGS WHERE LOWER(song_name) ILIKE '{song_name.lower()}'""").fetchone()
+		if cur.execute(f"""SELECT * FROM SONGS WHERE LOWER(song_name) LIKE '{song_name.lower()}'""").fetchone():
+			s = cur.execute(f"""SELECT * FROM SONGS WHERE LOWER(song_name) LIKE '{song_name.lower()}'""").fetchone()
 		else:
-			s = cur.execute(f"""SELECT * FROM SONGS WHERE LOWER(song_name) ILIKE '%{song_name.lower()}%'""").fetchone()
+			s = cur.execute(f"""SELECT * FROM SONGS WHERE LOWER(song_name) LIKE '%{song_name.lower()}%'""").fetchone()
 
 		if s:
 			f = cur.execute(f"""SELECT event_url FROM EVENTS WHERE event_date LIKE '{str(s[3])}'""").fetchone()
@@ -26,7 +26,7 @@ async def song_finder(ctx, *song):
 
 			opener = cur.execute(f"""SELECT COUNT(song_url) FROM SETLISTS WHERE song_url LIKE '%{s[1]}%' AND song_num=1 AND set_type NOT IN ('Soundcheck', 'Rehearsal')""").fetchone()
 			closer = cur.execute(f"""SELECT COUNT(event_url) FROM EVENTS WHERE setlist LIKE '%, {s[2].replace("'", "''")}'""").fetchone()
-			total = cur.execute("""SELECT COUNT(*) FROM EVENTS WHERE event_url LIKE '/gig:%'""").fetchone()
+			total = cur.execute("""SELECT COUNT(event_id) FROM EVENTS WHERE event_url LIKE '/gig:%'""").fetchone()
 			frequency = f"{round((s[5] / total[0] * 100), 2)}%"
 
 			embed = create_embed(s[2], f"[Brucebase Song Page]({main_url}{s[1]})", ctx)
