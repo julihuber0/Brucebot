29509730d05be467c1d65e92151aef2ece86dff1
Brian <bvw20002010@gmail.com>
2023-10-09 21:00:08 -0400

    deployment

--- create_embed.py
+++ create_embed.py
@@ -4,11 +4,15 @@ returns Discord embed
 """
 
 from import_stuff import discord
+import re
 
 def create_embed(title, description, ctx):
 	"""Returns a Discord Embed with the provided title and des."""
 	embed = discord.Embed(title=title, description=description, color=0x6d3da4)
 	embed.set_author(name=f"Requested by: {ctx.author.display_name}", icon_url=str(ctx.author.avatar.url))
-	embed.set_thumbnail(url=f"https://raw.githubusercontent.com/lilbud/lilbud.github.io/main/assets/img/graphics/bootleg-covers/features/bruce/{title[0:4]}.jpg")
+
+	eDate = re.findall("\d{4}", title)
+
+	embed.set_thumbnail(url=f"https://raw.githubusercontent.com/lilbud/lilbud.github.io/main/assets/img/graphics/bootleg-covers/features/bruce/{eDate[0]}.jpg")
 
 	return embed
