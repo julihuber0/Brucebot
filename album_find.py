3cc7d2065e776b1ba6b72771755bfd79d889f62c
Brian <bvw20002010@gmail.com>
2023-10-12 19:20:40 -0400

    deployment

--- album_find.py
+++ album_find.py
@@ -50,7 +50,7 @@ async def album_finder(ctx, *album):
             song_list = ", ".join(songs)
             embed.add_field(name="Songs (Bold = Not Played):", value=f"{song_list}", inline=False)
 
-            if plays[-1][1] and plays[0][1]:
+            if plays[-1] and plays[0]:
                 embed.add_field(name="Most/Least Played:", value=f"{plays[-1][0]} ({plays[-1][1]}) / {plays[0][0]} ({plays[0][1]})", inline=False)
 
             first_date = cur.execute(f"""SELECT event_date FROM EVENTS WHERE event_url LIKE '{premiere[0][1]}'""").fetchone()
