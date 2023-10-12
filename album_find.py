6725bf519d46b28e54379438e155adc5036afc5d
Brian <bvw20002010@gmail.com>
2023-10-12 01:01:58 -0400

    deployment

--- album_find.py
+++ album_find.py
@@ -84,7 +84,7 @@ async def album_finder(ctx, *album):
         first_date = cur.execute(f"""SELECT event_date FROM EVENTS WHERE event_url LIKE '{premiere[0][1]}'""").fetchone()
         last_date = cur.execute(f"""SELECT event_date FROM EVENTS WHERE event_url LIKE '{premiere[-1][1]}'""").fetchone()
 
-        embed.add_field(name="First/Last Premiered:", value=f"{premiere[0][0]} ([{main_url}{premiere[0][1]}]({first_date[0]})) / {premiere[-1][0]} ([{main_url}{premiere[-1][1]}]({last_date[0]}))", inline=False)
+        embed.add_field(name="First/Last Premiered:", value=f"{premiere[0][0]} ([{first_date[0]}]({main_url}{premiere[0][1]})) / {premiere[-1][0]} ([{last_date[0]}]({main_url}{premiere[-1][1]}))", inline=False)
 
         await ctx.send(embed=embed)
     else:
