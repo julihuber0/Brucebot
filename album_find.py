79f3a1772b1d308b71def8dc908bfc914e1d3471
Brian <bvw20002010@gmail.com>
2023-10-12 00:08:46 -0400

    deployment

--- album_find.py
+++ album_find.py
@@ -13,41 +13,37 @@ import re
 # nonukes, seeger, thepromise, tracks, tttb
 
 def album_name_fix(album): # wiess, btr, bitusa, tol, ht, lt, joad, woad, wb, hh, ws, lty, otss
-    if re.search(":*wiess:*", album, re.IGNORECASE):
+    if re.search("wiess", album, re.IGNORECASE):
         return "the wild, the innocent"
-    elif re.search(":*btr:*", album, re.IGNORECASE):
+    elif re.search("btr", album, re.IGNORECASE):
         return "born to run"
-    elif re.search(":*(bitusa| usa):*", album, re.IGNORECASE):
+    elif re.search("(bitusa| usa)", album, re.IGNORECASE):
         return "born in the u.s.a."
-    elif re.search(":*(tol|tunnel):*", album, re.IGNORECASE):
+    elif re.search("(tol|tunnel)", album, re.IGNORECASE):
         return "tunnel of love"
-    elif re.search(":*ht:*", album, re.IGNORECASE):
+    elif re.search("ht", album, re.IGNORECASE):
         return "human touch"
-    elif re.search(":*lt:*", album, re.IGNORECASE):
+    elif re.search("lt", album, re.IGNORECASE):
         return "lucky town"
-    elif re.search(":*gotj:*", album, re.IGNORECASE):
-        return "tom joad"
-    elif re.search(":*woad:*", album, re.IGNORECASE):
+    elif re.search("woad", album, re.IGNORECASE):
         return "working on a dream"
-    elif re.search(":*wb:*", album, re.IGNORECASE):
+    elif re.search("wb", album, re.IGNORECASE):
         return "wrecking ball"
-    elif re.search(":*hh:*", album, re.IGNORECASE):
+    elif re.search("hh", album, re.IGNORECASE):
         return "high hopes"
-    elif re.search(":*ws:*", album, re.IGNORECASE):
+    elif re.search("ws", album, re.IGNORECASE):
         return "western stars"
-    elif re.search(":*lty:*", album, re.IGNORECASE):
+    elif re.search("lty", album, re.IGNORECASE):
         return "letter to you"
-    elif re.search(":*otss:*", album, re.IGNORECASE):
+    elif re.search("otss", album, re.IGNORECASE):
         return "strong survive"
-    elif re.search(":*livenyc:*", album, re.IGNORECASE):
-        return "Live In New York City"
-    elif re.search(":*nonukes:*", album, re.IGNORECASE):
+    elif re.search("nonukes", album, re.IGNORECASE):
         return "no nukes"
-    elif re.search(":*seeger:*", album, re.IGNORECASE):
+    elif re.search("seeger", album, re.IGNORECASE):
         return "we shall overcome"
-    elif re.search(":*7585:*", album, re.IGNORECASE):
-        return "the promise"
-    elif re.search(":*tttb:*", album, re.IGNORECASE):
+    elif re.search("7585", album, re.IGNORECASE):
+        return "75-85"
+    elif re.search("tttb", album, re.IGNORECASE):
         return "the ties that bind"
     else:
         return album
@@ -58,19 +54,19 @@ async def album_finder(ctx, *album):
     """Gets info on album"""
     songs = []
 
-    for a in cur.execute(f"""SELECT DISTINCT(album_name) FROM ALBUMS""").fetchall():
-        if a[0].lower() == "".join(album).strip(":").lower():
-            album_to_find = a[0]
-        else:
-            album_to_find = album_name_fix(" ".join(album))
-    
+    # for a in cur.execute(f"""SELECT DISTINCT(album_name) FROM ALBUMS""").fetchall():
+    #     if a[0].lower() == album_name_fix(" ".join(album)):
+    #         album_to_find = a[0]
+    #     else:
+    #         await ctx.send(error_message("album"))
+
     # album name
     # year
     # songs
     # most played/least played
     # first/last premiered
 
-    album_info = cur.execute(f"""SELECT album_name, album_year, song_url FROM ALBUMS WHERE album_name LIKE '{album_to_find.replace("'", "''")}' ORDER BY song_num ASC""").fetchall()
+    album_info = cur.execute(f"""SELECT album_name, album_year, song_url FROM ALBUMS WHERE album_name LIKE '{" ".join(album).replace("'", "''")}' ORDER BY song_num ASC""").fetchall()
 
     if album_info:
         embed = create_embed(album_info[0][0], f"Year: {album_info[1]}", ctx)
