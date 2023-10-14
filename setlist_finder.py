f7d831176c12e81af076f7d03cfba143f1feff3c
Brian <bvw20002010@gmail.com>
2023-10-14 13:06:24 -0400

    deployment

--- setlist_finder.py
+++ setlist_finder.py
@@ -72,7 +72,7 @@ async def setlist_finder(ctx, date=None):
 						embed.add_field(name="", value=f"**Bootleg:** {bootleg}", inline=False)
 						embed.add_field(name="", value=f"**Official Release:** {official}", inline=False)
 					else:
-						embed.add_field(name=f"{s[4]}:", value="No Set Details Known", inline=False)
+						embed.add_field(name=f"{s[4]}:", value=setlist, inline=False)
 				
 				# embed.add_field(name="", value=f"**Bootleg:** {bootleg}", inline=False)
 				# embed.add_field(name="", value=f"**Official Release:** {official}", inline=False)
