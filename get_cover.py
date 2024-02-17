3490ee38ea6661c383c650c3542e359bc8167c85
Brian <bvw20002010@gmail.com>
2024-02-16 19:34:58 -0500

    add multiple images to embed

--- get_cover.py
+++ get_cover.py
@@ -3,8 +3,9 @@ get_cover
 returns covers from my site
 """
 
-import requests, re
-from import_stuff import bot, dateinDB
+import requests
+import re
+from import_stuff import bot, dateinDB, discord
 from error_message import error_message
 from bs4 import BeautifulSoup as bs4
 
@@ -16,18 +17,23 @@ async def get_cover(ctx, date=None):
     if dateinDB(date):
         links = []
         date_string = '{"name":"' + date
-        url = f"https://github.com/lilbud/Bootleg_Covers/raw/main/Bruce_Springsteen/covers/{date[0:4]}/"
+        git_url = f"https://github.com/lilbud/Bootleg_Covers/raw/main/Bruce_Springsteen/covers/{date[0:4]}/"
 
-        r = requests.get(url).text
+        r = requests.get(git_url).text
         soup = bs4(r, "lxml")
 
         for s in soup.text.split(","):
             if date_string in s:
                 for m in re.findall(f'{date}.*[^"]', s):
-                    links.append(f"{url}{m}")
+                    links.append(f"{git_url}{m}")
 
         if links:
-            await ctx.send("\n".join(links))
+            for link in links:
+                embed = discord.Embed(url="https://lilbud.github.io").set_image(
+                    url=link
+                )
+            # await ctx.send("\n".join(links))
+            await ctx.send(embed=embed)
         else:
             await ctx.send(error_message("cover"))
     else:
