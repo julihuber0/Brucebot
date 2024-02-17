97c6bd3543307b28dc2bfd1e3b9f646ff5e2e705
Brian <bvw20002010@gmail.com>
2024-02-16 19:53:21 -0500

    multiimage attempt

--- get_cover.py
+++ get_cover.py
@@ -5,9 +5,61 @@ returns covers from my site
 
 import requests
 import re
-from import_stuff import bot, dateinDB, discord
+from import_stuff import bot, dateinDB
 from error_message import error_message
 from bs4 import BeautifulSoup as bs4
+
+from discord import Webhook, AsyncWebhookAdapter, discord
+import aiohttp
+
+
+@bot.command()
+async def send_webhook(ctx, links):
+    async with aiohttp.ClientSession() as session:
+        webhook = Webhook.from_url("webhook_url", adapter=AsyncWebhookAdapter(session))
+
+        # e = discord.Embed(title="Title", description="Description")
+        # e.add_field(name="Field 1", value="Value 1")
+        # e.add_field(name="Field 2", value="Value 2")
+        match (len(links)):
+            case 1:
+                embed1 = discord.Embed(url="https://lilbud.github.io").set_image(
+                    url=links[0]
+                )
+                await webhook.send(embeds=[embed1])
+            case 2:
+                embed1 = discord.Embed(url="https://lilbud.github.io").set_image(
+                    url=links[0]
+                )
+                embed2 = discord.Embed(url="https://lilbud.github.io").set_image(
+                    url=links[1]
+                )
+                await webhook.send(embeds=[embed1, embed2])
+            case 3:
+                embed1 = discord.Embed(url="https://lilbud.github.io").set_image(
+                    url=links[0]
+                )
+                embed2 = discord.Embed(url="https://lilbud.github.io").set_image(
+                    url=links[1]
+                )
+                embed3 = discord.Embed(url="https://lilbud.github.io").set_image(
+                    url=links[2]
+                )
+                await webhook.send(embeds=[embed1, embed2, embed3])
+            case 4:
+                embed1 = discord.Embed(url="https://lilbud.github.io").set_image(
+                    url=links[0]
+                )
+                embed2 = discord.Embed(url="https://lilbud.github.io").set_image(
+                    url=links[1]
+                )
+                embed3 = discord.Embed(url="https://lilbud.github.io").set_image(
+                    url=links[2]
+                )
+                embed4 = discord.Embed(url="https://lilbud.github.io").set_image(
+                    url=links[3]
+                )
+                await webhook.send(embeds=[embed1, embed2, embed3, embed4])
 
 
 @bot.command(aliases=["cover", "getcover"])
@@ -26,47 +78,12 @@ async def get_cover(ctx, date=None):
             if date_string in s:
                 for m in re.findall(f'{date}.*[^"]', s):
                     links.append(f"{git_url}{m}")
+                    # embed1 = discord.Embed(url="https://lilbud.github.io").set_image(
+                    #     url=links[0]
+                    # )
 
-        if links:
-            match len(links):
-                case 1:
-                    embed1 = discord.Embed(url="https://lilbud.github.io").set_image(
-                        url=links[0]
-                    )
-                    await ctx.send(embed=embed1)
-                case 2:
-                    embed1 = discord.Embed(url="https://lilbud.github.io").set_image(
-                        url=links[0]
-                    )
-                    embed2 = discord.Embed(url="https://lilbud.github.io").set_image(
-                        url=links[1]
-                    )
-                    await ctx.send(embed=[embed1, embed2])
-                case 3:
-                    embed1 = discord.Embed(url="https://lilbud.github.io").set_image(
-                        url=links[0]
-                    )
-                    embed2 = discord.Embed(url="https://lilbud.github.io").set_image(
-                        url=links[1]
-                    )
-                    embed3 = discord.Embed(url="https://lilbud.github.io").set_image(
-                        url=links[2]
-                    )
-                    await ctx.send(embed=[embed1, embed2, embed3])
-                case 4:
-                    embed1 = discord.Embed(url="https://lilbud.github.io").set_image(
-                        url=links[0]
-                    )
-                    embed2 = discord.Embed(url="https://lilbud.github.io").set_image(
-                        url=links[1]
-                    )
-                    embed3 = discord.Embed(url="https://lilbud.github.io").set_image(
-                        url=links[2]
-                    )
-                    embed4 = discord.Embed(url="https://lilbud.github.io").set_image(
-                        url=links[3]
-                    )
-                    await ctx.send(embed=[embed1, embed2, embed3, embed4])
+            if links:
+                send_webhook(ctx, links)
 
             # await ctx.send("\n".join(links))
 
