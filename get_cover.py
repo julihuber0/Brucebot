5cf3c7639dd56c341c5f90615ec6aadc0a2e82b2
Brian <bvw20002010@gmail.com>
2024-03-26 13:31:40 -0400

    fix cover get

--- get_cover.py
+++ get_cover.py
@@ -16,23 +16,25 @@ async def get_cover(ctx, date=None):
     """Gets covers from my site based in input date"""
 
     if dateinDB(date):
-        links = []
-        url = f"https://github.com/lilbud/Bootleg_Covers/tree/main/Bruce_Springsteen/covers/{date[0:4]}"
+        base_url = "https://raw.githubusercontent.com/lilbud/Bootleg_Covers/main"
+        url = f"Bruce_Springsteen/covers/{date[0:4]}/{date}"
 
         with httpx.Client() as client:
             try:
-                r = client.get(url)
+                r = client.get(
+                    "https://api.github.com/repos/lilbud/Bootleg_Covers/git/trees/main?recursive=1",
+                    headers={
+                        "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"
+                    },
+                )
             except httpx.RequestError as exc:
                 print(f"An error occurred while requesting {exc.request.url!r}.")
 
-            soup = bs4(r.text, "lxml")
-            covers = json.loads(str(soup.find("p").text))["payload"]["tree"]["items"]
+            res = r.json()
 
-            for cover in covers:
-                if date in cover["path"]:
-                    links.append(
-                        f"https://raw.githubusercontent.com/lilbud/Bootleg_Covers/main/{cover['path']}"
-                    )
+            links = [
+                f"{base_url}/{img["path"]}" for img in res["tree"] if url in img["path"]
+            ]
 
             if links:
                 await ctx.send("\n".join(links))
