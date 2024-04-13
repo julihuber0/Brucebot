46b6fe48a5073bc583cdd38ef5852c1ab251247c
Brian <bvw20002010@gmail.com>
2024-04-12 21:35:53 -0400

    various fixes, type annotations and sqlite params

--- on_this_day.py
+++ on_this_day.py
@@ -1,39 +1,36 @@
-"""
-on_this_day
+"""on_this_day
 gets events based on inputted day
 or the current day if none specified
 """
 
-import re
 import datetime
-from import_stuff import bot, cur, main_url, cDate, location_name_get
+import re
+
 from create_embed import create_embed
 from error_message import error_message
+from import_stuff import bot, cur, current_date, location_name_get, main_url
 
 
 @bot.command(aliases=["otd", "onthisday"])
 async def on_this_day(ctx, *date):
-    """
-    Gets events based on specified month-day input
+    """Gets events based on specified month-day input
     or the current day if none specified
     """
-
     ndate = ""
 
     if not date:
-        ndate = f"-{(cDate.strftime('%m'))}-{cDate.strftime('%d')}"
-    else:
-        if re.search("\d{2}-\d{2}", date[0]):
-            ndate = f"-{str(date[0])}"
+        ndate = f"-{(current_date.strftime('%m'))}-{current_date.strftime('%d')}"
+    elif re.search(r"\d{2}-\d{2}", date[0]):
+        ndate = f"-{date[0]!s}"
 
     if ndate:
         otd_links = cur.execute(
-            f"""SELECT event_url, location_url, show, event_date FROM EVENTS WHERE event_date LIKE '%{ndate}' ORDER BY event_id ASC"""
+            f"""SELECT event_url, location_url, show, event_date FROM EVENTS WHERE event_date LIKE '%{ndate}' ORDER BY event_id ASC""",
         ).fetchall()
 
         embed = create_embed(
             f"On This Day: {datetime.datetime.strptime(ndate.strip('-'), '%m-%d').strftime('%B %d')}",
-            f"Number of Shows: {str(len(otd_links))}",
+            f"Number of Shows: {len(otd_links)!s}",
             ctx,
         )
 
